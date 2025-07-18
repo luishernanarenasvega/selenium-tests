3aa569b0ab6abfca89e6844a4e62c29c9c560bd5
Luis <luishernanarenasvega@gmail.com>
2025-07-17 21:55:40 -0400

    Fourth Commit

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 def test_page_title():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')  # Optional: headless for CI
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    
     browser = webdriver.Chrome(service=Service("C:\\Users\\Luis Arenas\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"))
     browser.get('https://github.com')
 
